ffecd97429381fa852194174aefe2148ed0775d4
Karl-Johan Karlsson <karl-johan.karlsson@liu.se>
2022-12-02 09:43:21 +0100

    Day 2 Ruby solutions

---
+++ 2-1.rb
@@ -0,0 +1,27 @@
+#!/usr/bin/env ruby
+
+strategies = File.read('2.input').lines.map(&:split)
+
+def score_round(other, me)
+  outcome_score = {
+    ['A', 'X'] => 3,
+    ['A', 'Y'] => 6,
+    ['A', 'Z'] => 0,
+    ['B', 'X'] => 0,
+    ['B', 'Y'] => 3,
+    ['B', 'Z'] => 6,
+    ['C', 'X'] => 6,
+    ['C', 'Y'] => 0,
+    ['C', 'Z'] => 3
+  }
+
+  play_score = {
+    'X' => 1,
+    'Y' => 2,
+    'Z' => 3
+  }
+
+  play_score[me] + outcome_score[[other, me]]
+end
+
+print strategies.map { |s| score_round s[0], s[1] }.reduce(:+), "\n"
