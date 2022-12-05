b69da8a5f56542063ef1c4ab1ff1a3085e7d17a5
Karl-Johan Karlsson <karl-johan.karlsson@liu.se>
2022-12-05 10:21:40 +0100

    Day 5 Python solutions

---
+++ 5-2.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+
+from collections import deque
+import re
+
+class Ship:
+    def __init__(self, stacks, last_stack):
+        self.num_stacks = last_stack + 1
+        self.ship = []
+        for i in range(self.num_stacks):
+            self.ship.append(deque())
+        for level in stacks:
+            stack = 0
+            for num in range(self.num_stacks - 1):
+                stack += 1
+                crate = level[num * 4 + 1]
+                if crate == " ":
+                    continue
+                self.ship[stack].appendleft(crate)
+
+    def move(self, num, source, dest):
+        temp = deque()
+        for _ in range(num):
+            temp.appendleft(self.ship[source].pop())
+        self.ship[dest].extend(temp)
+
+    def result(self):
+        res = ""
+        for stack in self.ship:
+            if len(stack) >= 1:
+                res += stack[-1]
+        return res
+
+    def __str__(self):
+        s = f"<{self.__class__.__name__}:\n"
+        for stack in self.ship[1:]:
+            s += str(stack)
+            s += "\n"
+        s += ">"
+        return s
+
+
+lines = open('5.input').readlines()
+
+number_format = re.compile("^(( [0-9]+ ) )+( [0-9]+ )$")
+move_format = re.compile("^move (?P<num>[0-9]+) from (?P<from>[0-9]+) to (?P<to>[0-9]+)$")
+
+ship = None
+stacks = []
+last_stack = 0
+
+for line in lines:
+    if "[" in line:
+        stacks.append(line)
+    elif number_format.match(line):
+        last_stack = int(line.split()[-1])
+        ship = Ship(stacks, last_stack)
+    elif (move := move_format.match(line.strip())):
+        ship.move(int(move['num']), int(move['from']), int(move['to']))
+    elif line == "\n":
+        pass
+
+print(ship.result())
