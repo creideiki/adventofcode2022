ace833ba233bdeaea0881d1d79e3911761d472dc
Karl-Johan Karlsson <karl-johan.karlsson@liu.se>
2022-12-07 10:22:55 +0100

    Day 7 Ruby solutions

---
+++ 7-2.rb
@@ -0,0 +1,80 @@
+#!/usr/bin/env ruby
+
+class Directory
+  attr_accessor :dirs, :files, :parent
+
+  def initialize(parent, name)
+    @parent = parent
+    @name = name
+    @dirs = {}
+    @files = {}
+    @size = -1
+  end
+
+  def size
+    if @size == -1
+      @size = (@dirs.values.map(&:size).reduce(:+) or 0) +
+              (@files.values.reduce(:+) or 0)
+    end
+    @size
+  end
+
+  def to_s
+    s = "<#{self.class}: #{@name} #{size}\n"
+    s += "Subdirectories:\n"
+    @dirs.each { |name, _| s += name + "\n" }
+    s += "Files:\n"
+    @files.each { |name, size| s += name + ": " + size.to_s + "\n" }
+    s += ">"
+  end
+
+  def inspect
+    to_s
+  end
+end
+
+cd_format = /^\$ cd (?<dir>[[:alnum:][:punct:]]+)$/
+ls_format = /^\$ ls$/
+dir_list_format = /^dir (?<name>[[:alnum:]]+)$/
+file_list_format = /^(?<size>[[:digit:]]+) (?<name>[[:alnum:][:punct:]]+)$/
+
+root = Directory.new(nil, '/')
+
+input = File.read('7.input').lines.map(&:strip)
+
+cwd = root
+
+input.each do |line|
+  if (match = cd_format.match line)
+    cwd = case match['dir']
+          when '/'
+            root
+          when '..'
+            cwd.parent
+          else
+            cwd.dirs[match['dir']]
+          end
+  elsif ls_format.match line
+    next
+  elsif (match = dir_list_format.match line)
+    cwd.dirs[match['name']] = Directory.new(cwd, match['name'])
+  elsif (match = file_list_format.match line)
+    cwd.files[match['name']] = match['size'].to_i
+  end
+end
+
+min_to_delete = 30_000_000 - (70_000_000 - root.size)
+
+current_smallest_valid = root
+to_check = [root]
+
+while (cwd = to_check.pop)
+  if cwd.size > min_to_delete and
+     cwd.size < current_smallest_valid.size
+    current_smallest_valid = cwd
+  end
+
+  to_check.concat cwd.dirs.values
+end
+
+print current_smallest_valid.size, "\n"
