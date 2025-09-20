5543ad579c905ea8e4460e4706343496ff74cc74
Karl-Johan Karlsson <creideiki@ferretporn.se>
2025-09-20 11:26:10 +0200

    Replace repository with notice of move to Codeberg

--- 4-1.rb
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env ruby
-
-assignments = File.read('4.input').lines.map(&:strip)
-                .map { |s| s.split(',').map { |a| a.split('-').map(&:to_i) } }
-overlap = assignments.map do |a|
-  (a[0][0] <= a[1][0] and a[0][1] >= a[1][1]) or
-    (a[1][0] <= a[0][0] and a[1][1] >= a[0][1])
-end
-print overlap.count { |a| a }, "\n"
